399d0f6d74c4cd8abf541cee20570eab4baf2ce4
kysCode <113599171+kysCode@users.noreply.github.com>
2022-11-15 10:36:49 +0000

    Changed body movement conditions
    Body would move to the left when it needed to move to the right

--- main.py
+++ main.py
@@ -192,19 +192,19 @@ while run:
       if snake.left_pressed or snake.right_pressed or snake.up_pressed or snake.down_pressed:
         # deciding when each part of the snake in the array must change direction
         for i in range(1, len(player)):
-          if player[i].x < player[i - 1].x:
+          if player[i].x > player[i - 1].x:
             player[i].left_pressed = True
             player[i].up_pressed = False
             player[i].down_pressed = False
-          elif player[i].x > player[i - 1].x:
+          elif player[i].x < player[i - 1].x:
             player[i].right_pressed = True
             player[i].up_pressed = False
             player[i].down_pressed = False
-          elif player[i].y > player[i - 1].y:
+          elif player[i].y < player[i - 1].y:
             player[i].up_pressed = True
             player[i].right_pressed = False
             player[i].left_pressed = False
-          elif player[i].y < player[i - 1].y:
+          elif player[i].y > player[i - 1].y:
             player[i].down_pressed = True
             player[i].right_pressed = False
             player[i].left_pressed = False
